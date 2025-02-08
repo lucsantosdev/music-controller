246b6daa50dfab7f07f51337b2f86c73be308ad5
Lucas Santos <lucsantosdev@gmail.com>
2025-02-08 10:32:00 -0300

    creation of icons components

---
+++ components/icons/EmptyAlbumIcon.jsx
@@ -0,0 +1,5 @@
+export default function EmptyAlbumIcon() {
+    return (
+        <div className="w-12 h-12 bg-neutral-500"></div>
+    )
+}
---
+++ components/icons/PlayIcon.jsx
@@ -0,0 +1,18 @@
+export default function PlayIcon({onClick}) {
+    return (
+        <svg
+          onClick={onClick}
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"          
+        >
+          <path
+            d="M6.26693 21.4878L6.25583 21.4955L6.24514 21.5037C5,95991 21.7231 5.5 21.5312 5.5 21.1V2.9C5.5 2.4688 5.95991 2.2769 6.24514 2.49631L6.25634 2.50492L6.26799 2.51288L19.51" // incomplete
+            fill="#FCFCFC"
+            stroke="#FCFCFC"
+          />
+        </svg>
+    )
+}
